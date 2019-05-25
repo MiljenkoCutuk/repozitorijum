6d478735eed0e67dbbdf5b92f9591fa2579e36bd
Mihailo Ristivojevic <mihailo2910@gmail.com>
2019-05-25 18:55:24 +0200

    Ovo je prvi commit

---
+++ Apple.java
@@ -0,0 +1,11 @@
+public class Apple{
+	
+	public static void main(String args[]){
+		
+		System.out.println("Test za git projekat");
+	}
+	public void uradi(){
+		Tuna t = new Tuna();
+		tuna.stampaj();
+	}
+}
---
+++ Tuna.java
@@ -0,0 +1,5 @@
+public class Tuna{
+	public void stampaj(){
+		System.out.println("Ovo je druga klasa");
+	}
+}
