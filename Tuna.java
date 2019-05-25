c85b30160b0c52f595fe1b96f70bba65623ea65f
Mihailo Ristivojevic <mihailo2910@gmail.com>
2019-05-25 19:28:31 +0200

    treci commit po redu

--- Tuna.java
+++ Tuna.java
@@ -2,5 +2,6 @@ public class Tuna{
 	public void stampaj(){
 		System.out.println("Ovo je druga klasa");
 		System.out.println("Ovo je dodato nakon prvog commita");
+		System.out.println("Ovo je dodato najkasnije");
 	}
 }
